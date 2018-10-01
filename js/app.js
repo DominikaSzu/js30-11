7a138da95e9c9b5c0cea36b08efd4501134750f4
Dominika Szukała <dominika.szukala@gmail.com>
2018-10-01 14:55:47 +0200

    Video is pausing and playing

---
+++ js/app.js
@@ -0,0 +1,14 @@
+const player = document.querySelector('.player');
+const playerBtn = player.querySelector('.player__button.toggle');
+const videoPlayer = document.querySelector('.player__video');
+
+function playItSam() {
+
+    if (videoPlayer.paused) {
+        videoPlayer.play()
+    } else {
+        videoPlayer.pause();
+    }
+}
+
+playerBtn.addEventListener('click', playItSam);
