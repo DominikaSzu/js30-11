0a5ba6f61b2d79603acf3e1dc097c348723cb75c
Dominika Szukała <dominika.szukala@gmail.com>
2018-10-02 15:03:07 +0200

    Skipping video works

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 const player = document.querySelector('.player');
 const playerBtn = player.querySelector('.player__button.toggle');
 const videoPlayer = document.querySelector('.player__video');
+const skipButtons = document.querySelectorAll('[data-skip]');
 
 function playItSam() {
 
@@ -17,7 +18,13 @@ function updateBtn() {
     playerBtn.textContent = icon; 
 }
 
+function skipVideo() {
+    let timeToSkip = this.dataset.skip;
+    videoPlayer.currentTime += parseFloat(timeToSkip);
+}
+
 playerBtn.addEventListener('click', playItSam);
 videoPlayer.addEventListener('click', playItSam);
 videoPlayer.addEventListener('play', updateBtn);
 videoPlayer.addEventListener('pause', updateBtn);
+skipButtons.forEach(skipBtn => skipBtn.addEventListener('click', skipVideo));
