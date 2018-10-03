4f2613fc568a4223f986f327e047063d4ecfbbc6
Dominika Szukała <dominika.szukala@gmail.com>
2018-10-03 11:18:23 +0200

    Playback rate enabled

--- js/app.js
+++ js/app.js
@@ -2,6 +2,9 @@ const player = document.querySelector('.player');
 const playerBtn = player.querySelector('.player__button.toggle');
 const videoPlayer = document.querySelector('.player__video');
 const skipButtons = document.querySelectorAll('[data-skip]');
+const playbackRate = document.getElementsByName('playbackRate').item(0);
+
+// Playing the video
 
 function playItSam() {
 
@@ -10,21 +13,34 @@ function playItSam() {
     } else {
         videoPlayer.pause();
     }
-
 }
+
+// Updating textContent of a play-pause button
 
 function updateBtn() {
     let icon = this.paused ? '▶' : '| |';
     playerBtn.textContent = icon; 
 }
 
+// Skipping video forward and backward
+
 function skipVideo() {
     let timeToSkip = this.dataset.skip;
     videoPlayer.currentTime += parseFloat(timeToSkip);
 }
+
+// Changing the playback rate of video
+
+function changePlaybackRate () {
+    let speed = this.value;
+    videoPlayer.playbackRate = speed;
+}
+
+// Event listeners
 
 playerBtn.addEventListener('click', playItSam);
 videoPlayer.addEventListener('click', playItSam);
 videoPlayer.addEventListener('play', updateBtn);
 videoPlayer.addEventListener('pause', updateBtn);
 skipButtons.forEach(skipBtn => skipBtn.addEventListener('click', skipVideo));
+playbackRate.addEventListener('input', changePlaybackRate);
