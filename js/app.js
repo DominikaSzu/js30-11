ea77067c30a7c31ed4e0137a008b62e8cb05c719
Dominika Szukała <dominika.szukala@gmail.com>
2018-10-03 11:21:26 +0200

    Volume change enabled

--- js/app.js
+++ js/app.js
@@ -3,6 +3,7 @@ const playerBtn = player.querySelector('.player__button.toggle');
 const videoPlayer = document.querySelector('.player__video');
 const skipButtons = document.querySelectorAll('[data-skip]');
 const playbackRate = document.getElementsByName('playbackRate').item(0);
+const volumeRate = document.getElementsByName('volume').item(0);
 
 // Playing the video
 
@@ -36,6 +37,13 @@ function changePlaybackRate () {
     videoPlayer.playbackRate = speed;
 }
 
+// Changing the volume of the video
+
+function changeVolume () {
+    let volume = this.value;
+    videoPlayer.volume = volume;
+}
+
 // Event listeners
 
 playerBtn.addEventListener('click', playItSam);
@@ -44,3 +52,4 @@ videoPlayer.addEventListener('play', updateBtn);
 videoPlayer.addEventListener('pause', updateBtn);
 skipButtons.forEach(skipBtn => skipBtn.addEventListener('click', skipVideo));
 playbackRate.addEventListener('input', changePlaybackRate);
+volumeRate.addEventListener('input', changeVolume);
